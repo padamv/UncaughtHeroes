b336c0a208c58fbe32273ea55df574f7f5237b54
bczovek <czovekbence01@gmail.com>
2023-07-05 11:35:19 +0200

    NPT-14 - Original HeroController made by ChatGPT with minor adjustments

---
+++ src/main/java/com/npe/pet/uncaughtHeroes/controller/HeroController.java
@@ -0,0 +1,54 @@
+package com.npe.pet.uncaughtHeroes.controller;
+
+import com.npe.pet.uncaughtHeroes.entity.Hero;
+import com.npe.pet.uncaughtHeroes.service.HeroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/heroes")
+public class HeroController {
+
+    private final HeroService heroService;
+
+    @Autowired
+    public HeroController(HeroService heroService) {
+        this.heroService = heroService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Hero> saveHero(@RequestBody Hero hero) {
+        Hero savedHero = heroService.save(hero);
+        return new ResponseEntity<>(savedHero, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Hero> getHeroById(@PathVariable("id") Long id) {
+        Hero hero = heroService.findById(id);
+        if (hero != null) {
+            return ResponseEntity.ok(hero);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public List<Hero> getAllHeroes() {
+        return heroService.findAll();
+    }
+
+    @GetMapping("/strength/{value}")
+    public List<Hero> getHeroesWithStrengthGreaterThan(@PathVariable("value") int value) {
+        return heroService.findByStrengthGreaterThan(value);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteHeroById(@PathVariable("id") Long id) {
+        heroService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
