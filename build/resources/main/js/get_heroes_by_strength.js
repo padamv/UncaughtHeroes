406e0b2ce997ed44480d3f8101d8bef9203b0434
fazdate <fazdate15@gmail.com>
2023-11-13 16:45:51 +0100

    NPT-38: Switched from Gradle to Maven

---
+++ build/resources/main/js/get_heroes_by_strength.js
@@ -0,0 +1,54 @@
+const apiUrl = `http://localhost:8080/api/heroes?strengthGreaterThan=`;
+
+function createHeroCard(hero) {
+  var card = `
+    <div class="col-md-4 mb-4">
+      <a href="../html/view_hero.html?id=${hero.id}" class="card-link">
+        <div class="card">
+          <img src="${hero.picturePath}" onerror="this.src='../images/hero_placeholder.png'" class="card-img-top" alt="${hero.name}">
+          <div class="card-body">
+            <h5 class="card-title">${hero.name}</h5>
+            <p class="card-text">${hero.description}</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  `;
+  return card;
+}
+
+function renderHeroesList(heroesData) {
+  const heroesList = $("#heroesList");
+  heroesList.empty();
+
+  heroesData.forEach(function (hero) {
+    const heroCard = createHeroCard(hero);
+    heroesList.append(heroCard);
+  });
+}
+
+function fetchHeroesData(apiUrl) {
+  $.ajax({
+    url: apiUrl,
+    method: "GET",
+    dataType: "json",
+    success: function (data) {
+      renderHeroesList(data);
+    },
+    error: function (error) {
+      console.error("Error fetching heroes data:", error);
+    },
+  });
+}
+
+function findHeroesByStrength() {
+  var strengthValue = parseInt(document.getElementById("strengthValue").value);
+  var updatedApiUrl = apiUrl + strengthValue;
+  fetchHeroesData(updatedApiUrl);
+}
+
+
+document.getElementById("strengthForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+  findHeroesByStrength();
+});
---
+++ build/resources/main/js/view_hero.js
@@ -0,0 +1,56 @@
+const queryParams = new URLSearchParams(window.location.search);
+const heroId = queryParams.get("id");
+const apiUrl = `http://localhost:8080/api/heroes/${heroId}`;
+
+function fetchHeroesData() {
+	$.ajax({
+		url: apiUrl,
+		method: "GET",
+		dataType: "json",
+		success: function (data) {
+			populateHeroDetails(data);
+		},
+		error: function () {
+			showErrorAndNavigateBack();
+		},
+	});
+}
+
+function populateHeroDetails(heroData) {
+	var heroImageElement = document.getElementById("heroImage");
+	heroImageElement.src = "../" + heroData.picturePath;
+	heroImageElement.onerror = function () {
+		this.src = "../images/hero_placeholder.png";
+	};
+
+	document.getElementById("heroName").textContent = heroData.name;
+	document.getElementById("heroDescription").textContent = heroData.description;
+	document.getElementById("strength").textContent = heroData.strength;
+	document.getElementById("dexterity").textContent = heroData.dexterity;
+	document.getElementById("constitution").textContent = heroData.constitution;
+	document.getElementById("intelligence").textContent = heroData.intelligence;
+	document.getElementById("wisdom").textContent = heroData.wisdom;
+	document.getElementById("speed").textContent = heroData.speed;
+	document.getElementById("charisma").textContent = heroData.charisma;
+	document.getElementById("xp").textContent = heroData.xp;
+	document.getElementById("mp").textContent = heroData.mp;
+	document.getElementById("hp").textContent = heroData.hp;
+	document.getElementById("baseAttack").textContent = heroData.baseAttack;
+	document.getElementById("baseDefense").textContent = heroData.baseDefense;
+	document.getElementById("maxSpells").textContent = heroData.maxSpells;
+	document.getElementById("carryingCapacity").textContent = heroData.carryingCapacity;
+	document.getElementById("weight").textContent = heroData.weight;
+	document.getElementById("height").textContent = heroData.height;
+	document.getElementById("race").textContent = heroData.race;
+	document.getElementById("skills").textContent = heroData.skills.join(", ");
+	document.getElementById("weapons").textContent = heroData.weapons.join(", ");
+	document.getElementById("spells").textContent = heroData.spellList.join(", ");
+	document.getElementById("items").textContent = heroData.items.join(", ");
+}
+
+function showErrorAndNavigateBack() {
+	alert("Error: Hero not found!");
+	window.history.back();
+}
+
+fetchHeroesData();
